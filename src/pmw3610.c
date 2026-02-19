11056bc2a62c5f23e34d996b3f35503513e1ee9c
iwk7273 <iwk.nov16@gmail.com>
2026-02-20 01:01:20 +0900

    Revert "Queue CPI until PMW3610 ready"
    This reverts commit e51bc0007b89e7aaecf10a9e106c2a5a32a49920.

--- src/pmw3610.c
+++ src/pmw3610.c
@@ -367,10 +367,10 @@ static int pmw3610_async_init_configure(const struct device *dev) {
     return 0;
 }
 
-  static void pmw3610_async_init(struct k_work *work) {
-      struct k_work_delayable *work2 = (struct k_work_delayable *)work;
-      struct pixart_data *data = CONTAINER_OF(work2, struct pixart_data, init_work);
-      const struct device *dev = data->dev;
+static void pmw3610_async_init(struct k_work *work) {
+    struct k_work_delayable *work2 = (struct k_work_delayable *)work;
+    struct pixart_data *data = CONTAINER_OF(work2, struct pixart_data, init_work);
+    const struct device *dev = data->dev;
 
     LOG_INF("PMW3610 async init step %d", data->async_init_step);
 
@@ -380,25 +380,15 @@ static int pmw3610_async_init_configure(const struct device *dev) {
     } else {
         data->async_init_step++;
 
-          if (data->async_init_step == ASYNC_INIT_STEP_COUNT) {
-              data->ready = true; // sensor is ready to work
-              LOG_INF("PMW3610 initialized");
-              pmw3610_set_interrupt(dev, true);
-              uint32_t pending_cpi = (uint32_t)atomic_get(&data->pending_cpi);
-              if (pending_cpi != 0) {
-                  int err = pmw3610_set_cpi(dev, pending_cpi);
-                  if (err) {
-                      LOG_WRN("Failed to apply pending CPI %u (%d)", pending_cpi, err);
-                  } else {
-                      atomic_set(&data->pending_cpi, 0);
-                      LOG_INF("Applied pending CPI %u", pending_cpi);
-                  }
-              }
-          } else {
-              k_work_schedule(&data->init_work, K_MSEC(async_init_delay[data->async_init_step]));
-          }
-      }
-  }
+        if (data->async_init_step == ASYNC_INIT_STEP_COUNT) {
+            data->ready = true; // sensor is ready to work
+            LOG_INF("PMW3610 initialized");
+            pmw3610_set_interrupt(dev, true);
+        } else {
+            k_work_schedule(&data->init_work, K_MSEC(async_init_delay[data->async_init_step]));
+        }
+    }
+}
 
 static int pmw3610_report_data(const struct device *dev) {
     struct pixart_data *data = dev->data;
@@ -545,19 +535,18 @@ static int pmw3610_init_irq(const struct device *dev) {
     return err;
 }
 
-  static int pmw3610_init(const struct device *dev) {
-      struct pixart_data *data = dev->data;
-      const struct pixart_config *config = dev->config;
-      int err;
+static int pmw3610_init(const struct device *dev) {
+    struct pixart_data *data = dev->data;
+    const struct pixart_config *config = dev->config;
+    int err;
 
 	if (!spi_is_ready_dt(&config->spi)) {
 		LOG_ERR("%s is not ready", config->spi.bus->name);
 		return -ENODEV;
 	}
 
-      // init device pointer
-      data->dev = dev;
-      atomic_set(&data->pending_cpi, 0);
+    // init device pointer
+    data->dev = dev;
 
     // init smart algorithm flag;
     data->sw_smart_flag = false;
@@ -583,25 +572,19 @@ static int pmw3610_init_irq(const struct device *dev) {
     return err;
 }
 
-  static int pmw3610_attr_set(const struct device *dev, enum sensor_channel chan,
-                              enum sensor_attribute attr, const struct sensor_value *val) {
-      struct pixart_data *data = dev->data;
-      int err;
+static int pmw3610_attr_set(const struct device *dev, enum sensor_channel chan,
+                            enum sensor_attribute attr, const struct sensor_value *val) {
+    struct pixart_data *data = dev->data;
+    int err;
 
-      if (unlikely(chan != SENSOR_CHAN_ALL)) {
-          return -ENOTSUP;
-      }
+    if (unlikely(chan != SENSOR_CHAN_ALL)) {
+        return -ENOTSUP;
+    }
 
-      if (unlikely(!data->ready)) {
-          if ((uint32_t)attr == PMW3610_ATTR_CPI) {
-              uint32_t cpi = PMW3610_SVALUE_TO_CPI(*val);
-              atomic_set(&data->pending_cpi, (atomic_val_t)cpi);
-              LOG_INF("Queued CPI %u", cpi);
-              return 0;
-          }
-          LOG_DBG("Device is not initialized yet");
-          return -EBUSY;
-      }
+    if (unlikely(!data->ready)) {
+        LOG_DBG("Device is not initialized yet");
+        return -EBUSY;
+    }
 
     switch ((uint32_t)attr) {
     case PMW3610_ATTR_CPI:
