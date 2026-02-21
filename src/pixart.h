8af03264f272dd709f50cca8b80c835294e1f320
iwk7273 <iwk.nov16@gmail.com>
2026-02-21 15:03:13 +0900

    Add pending CPI field to pixart_data

--- src/pixart.h
+++ src/pixart.h
@@ -10,6 +10,7 @@
 #include <zephyr/drivers/spi.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/sensor.h>
+#include <zephyr/sys/atomic.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -29,6 +30,7 @@ struct pixart_data {
 
     bool                         ready; // whether init is finished successfully
     int                          err; // error code during async init
+    atomic_t                     pending_cpi; // pending CPI before ready (0 = none)
 };
 
 // device config data structure
